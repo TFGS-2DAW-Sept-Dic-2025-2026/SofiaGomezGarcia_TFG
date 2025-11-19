fe9f97692121e56824657ff068e8ab7db0bb909d
Sofia Gomez Garcia <sofiiagomezgarciia15@gmail.com>
2025-11-19 19:32:44 +0100

    Primera version home pagina bienvenida sin iniciar sesion

--- angular/src/app/componentes/home/home.component.ts
+++ angular/src/app/componentes/home/home.component.ts
@@ -16,46 +16,78 @@ export class HomeComponent {
   servicioSeries = inject(seriesService);
 
   isLoggedIn = false;
+
   series: any[] = [];
   currentPage = 1;
   totalPages = 1;
   loading = false;
+  initialized = false;
 
   constructor() {
-    
-    if (this.auth.hasValidSession()) {
-      this.loadSeries();
+  effect(() => {
+    this.isLoggedIn = this.auth.hasValidSession();
+
+    if (this.isLoggedIn && !this.initialized) {
+      this.initialized = true;
+      this.resetAndLoadTopSeries();
     }
 
-    
-    effect(() => {
-      this.isLoggedIn = this.auth.hasValidSession();
-      if (this.isLoggedIn) {
-        this.loadSeries();
-      }
-    });
+    if (!this.isLoggedIn && !this.initialized) {
+      this.initialized = true;
+      this.cargarSoloPrimeraPagina();  
+    }
+  });
+}
+
+ 
+  resetAndLoadTopSeries() {
+    this.series = [];
+    this.currentPage = 1;
+    this.totalPages = 1;
+    this.loadSeries();
   }
 
+ 
   loadSeries() {
-    if (this.loading || this.currentPage > this.totalPages) return;
+  if (!this.isLoggedIn) return;  
+  if (this.loading || this.currentPage > this.totalPages) return;
 
-    this.loading = true;
-    this.servicioSeries.getSeries(this.currentPage).subscribe({
-      next: (res) => {
-        this.series.push(...res.results);
-        this.totalPages = res.total_pages;
-        this.currentPage++;
-        this.loading = false;
-      },
-      error: (err) => {
-        console.error('Error cargando series:', err);
-        this.loading = false;
-      }
-    });
-  }
+  this.loading = true;
+
+  this.servicioSeries.getSeries(this.currentPage).subscribe({
+    next: (res) => {
+      this.series.push(...res.results);
+      this.totalPages = res.total_pages;
+      this.currentPage++;
+      this.loading = false;
+    },
+    error: (err) => {
+      console.error('Error cargando series:', err);
+      this.loading = false;
+    }
+  });
+}
+
+  cargarSoloPrimeraPagina() {
+  this.loading = true;
+
+  
+  this.servicioSeries.getSeries(1).subscribe({
+    next: (res) => {
+      this.series = res.results;  
+      this.loading = false;
+    },
+    error: (err) => {
+      console.error('Error cargando series iniciales:', err);
+      this.loading = false;
+    }
+  });
+}
 
   @HostListener('window:scroll', [])
   onScroll(): void {
+    if (!this.isLoggedIn) return;  
+
     const scrollTop = window.scrollY;
     const windowHeight = window.innerHeight;
     const documentHeight = document.documentElement.scrollHeight;
